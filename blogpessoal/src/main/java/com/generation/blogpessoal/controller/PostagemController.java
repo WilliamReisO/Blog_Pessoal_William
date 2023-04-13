27c72e68cfd95206c328876ed5d557ad30432c44
WilliamReisO <oliveira._r@hotmail.com>
2023-04-13 15:55:01 -0300

    commit correção getmapping

--- blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
-
 @RestController
 @RequestMapping("/Postagens")
 @CrossOrigin(origins = "*",allowedHeaders = "*")
@@ -19,4 +19,9 @@ public class PostagemController {
 
 @Autowired
 private PostagemRepository postagemRepository;
+
+@GetMapping
+public ResponseEntity<List<Postagem>> getAll(){
+	return ResponseEntity.ok(postagemRepository.findAll());
+}
 }
