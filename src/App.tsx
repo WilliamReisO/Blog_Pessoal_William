6388bbe21617c118ff1af4d5a04b1231b65b07b6
WilliamReisO <oliveira._r@hotmail.com>
2023-05-12 02:12:45 -0300

    Criando o front em em React

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './paginas/home/Home.css'
+import Home from './paginas/home/Home'; 
+
+function Home() {
+  return (
+    <Home/>
+  )
+}
+
+export default Home
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <div className="home">
+        <h1 className="titulo">Teste</h1>
+        <img src ="/img/IwWBH3.jpg" alt = "Imagem inicial teste" className='img'/>
+        </div>
+    );
+}
+export default Home;
